3b45a0e639c1f84b705d46423c4f59d52874c54c
stfnsfr <83638386+stfnsfr@users.noreply.github.com>
2021-05-04 18:24:31 +0200

    d

--- get-quote.py
+++ get-quote.py
@@ -7,7 +7,10 @@ def primary():
   f.close()
   last = 13
   rnd = random.randint(0, last)
+  rnd2 = random.randint(0, last)
   print(quotes[rnd])
+  print(quotes[rnd2])
+ # print(values='TEST'[last+1])
 
 if __name__== "__main__":
   primary()
